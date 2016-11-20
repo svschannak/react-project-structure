3f171efaa15d1e1bfad7d3b3a3b5c5d975a7d137
Sven Schannak <EPATROL-Sven@Svens-iMac.fritz.box>
2016-11-20 16:53:10 +0100

    reducer bug fixed

--- reducer.js
+++ reducer.js
@@ -4,10 +4,12 @@ import * as settings from './appConfig';
 let app_reducers = {};
 
 settings.INSTALLED_APPS.forEach((app) => {
-    app_reducers[app.BaseReducer.name] = app.Reducer;
+    app.ModuleReducer.forEach((reducer) => {
+        app_reducers[reducer.name] = reducer;
+    });
 });
 
 
 const BaseReducer = combineReducers(app_reducers);
 
-export default BaseReducer
+export default BaseReducer;
